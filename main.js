023b7e22531d610cfc859c41d866f09cd3875f43
kiqprado <kiqprado@outlook.com>
2022-11-07 06:07:08 -0300

    Calendário da Copa

---
+++ main.js
@@ -0,0 +1,32 @@
+function createGame(player1, hour, player2) {
+  return `
+    <li>
+      <img src="./assets/icon-${player1}.svg " alt=" Bandeira do ${player1} " />
+      <strong> ${hour} </strong>
+      <img src="./assets/icon-${player2}.svg " alt=" Bandeira do ${player2} " />
+    </li>
+  `
+}
+
+let delay = -0.3;
+function createCard(date, day, games) {
+  delay = delay + 0.3;
+  return `
+    <div class= "card" style= "animation-delay: ${delay}s" >
+      <h2> ${date} <span> ${day} </span></h2>
+      <ul>
+        ${games}
+      </ul>
+    </div>
+  `
+}
+
+document.querySelector("#cards").innerHTML =
+  createCard(
+    "24/11",
+    "quinta",
+    createGame("switzerland", "07:00", "cameroon") +
+      createGame("switzerland", "07:00", "cameroon")
+  ) +
+  createCard("28/11", "segunda", createGame("qatar", "13:00", "tunisia")) +
+  createCard("28/11", "sexta", createGame("portugal", "16:00", "senegal"))
